b8054c6dee0e53982269ec6669a019677fad6e14
ElcioLS <elciols@gmail.com>
2022-12-17 16:39:48 -0300

    Layout Header buscar filme

--- lib/modules/movies/movies_page.dart
+++ lib/modules/movies/movies_page.dart
@@ -1,12 +1,20 @@
+import 'package:adf_filmes/modules/movies/widgets/movies_header.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MoviesPage extends StatelessWidget {
   const MoviesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Fimes')),
+    return SizedBox(
+      width: Get.width,
+      // height: Get.height,
+      child: ListView(
+        children: const [
+          MoviesHeader(),
+        ],
+      ),
     );
   }
 }
